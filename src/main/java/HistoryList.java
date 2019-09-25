5fe2b084a3a43d530d71f25680f2cc8da806e74a
suaadBatis <we_oo@hotmail.com>
2019-09-25 17:52:17 +0300

    adding new log sercives

--- src/main/java/HistoryList.java
+++ src/main/java/HistoryList.java
@@ -1,18 +1,31 @@
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.service.local.flags.GeneralServerFlag;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.junit.After;
+
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class HistoryList {
-
+        public  static AppiumDriverLocalService service;
         private AndroidDriver driver;
 
         @Before
         public void setUp() throws MalformedURLException {
+
+            service = AppiumDriverLocalService.buildService (
+                    new AppiumServiceBuilder ().usingDriverExecutable(new File ("/usr/local/bin/node"))
+                            .withAppiumJS(new File("/Applications/Appium.app/Contents/Resources/app/node_modules/appium/build/lib/main.js"))
+                            .withArgument(GeneralServerFlag.LOCAL_TIMEZONE)
+                            .withLogFile(new File(System.getProperty("user.dir")+"/src/test/resources/Lsogs/Appium1.log")));
+            service.start (); // check the logs folder
+
             DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
             desiredCapabilities.setCapability("platformName", "android");
             desiredCapabilities.setCapability("app", "/Users/suaadbatis/Desktop/Appiumpro/app-release.apk");
@@ -24,17 +37,17 @@ public class HistoryList {
             driver = new AndroidDriver(remoteUrl, desiredCapabilities);
         }
 
-
         @Test
         public void sampleTest() {
             MobileElement el1 = (MobileElement) driver.findElementByXPath("(//android.widget.ImageView[@content-desc=\"Banner\"])[1]");
             el1.click();
         }
 
-
         @After
         public void tearDown() {
+
             driver.quit();
+            service.stop ();
         }
     }
 
