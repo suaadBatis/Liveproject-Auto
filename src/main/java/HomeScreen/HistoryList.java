7e0a204fee0298d7e6819779b8db5f01e9474456
suaadBatis <we_oo@hotmail.com>
2019-09-25 20:23:41 +0300

    adding logs file for each class

--- src/main/java/HomeScreen/HistoryList.java
+++ src/main/java/HomeScreen/HistoryList.java
@@ -3,21 +3,31 @@ package HomeScreen;
 
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.service.local.flags.GeneralServerFlag;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class HistoryList {
-
+    public  static AppiumDriverLocalService service;
     private AndroidDriver driver;
 
     @Before
     public void setUp () throws MalformedURLException {
+        service = AppiumDriverLocalService.buildService (
+                new AppiumServiceBuilder ().usingDriverExecutable(new File ("/usr/local/bin/node"))
+                        .withAppiumJS(new File("/Applications/Appium.app/Contents/Resources/app/node_modules/appium/build/lib/main.js"))
+                        .withArgument(GeneralServerFlag.LOCAL_TIMEZONE)
+                        .withLogFile(new File (System.getProperty("user.dir")+"/src/test/resources/Lsogs/Appium1.log")));
+        service.start (); // check the logs folder
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities ();
         desiredCapabilities.setCapability ("platformName", "android");
         desiredCapabilities.setCapability ("platformVersion", "8.0");
@@ -40,7 +50,9 @@ public class HistoryList {
 
     @After
     public void tearDown() {
+
         driver.quit();
+        service.stop ();
     }
 
 }
